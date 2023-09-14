d581d41c06faeabbd8434efcba4c61d005c5de5a
peipeix2 <peipei.hsueh@outlook.com>
2023-09-14 21:50:19 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+const cookieParser = require('cookie-parser')
+
+app.use(express.static('public'))
+app.use(cookieParser())
+
+app.set('view engine', 'pug')
+
+// Solution to Assignment-1
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World. This is the solution to Week-3 Assignment 1.</h1>')
+})
+
+// Solution to Assignment-2
+app.get('/getData', (req, res) => {
+  const number = req.query.number
+  if (!number) {
+    return res.send('Lack of Parameter.')
+  } else if (isNaN(number)) {
+    return res.send('Wrong Parameter.')
+  }
+
+  let sum = 0
+  for (let i = 1; i <= number; i++) {
+    sum += i
+  }
+  res.send(`${sum}`)
+})
+
+// Solution to Assignment-5
+app.get('/myName', (req, res) => {
+  const { username } = req.cookies
+  if (!username) {
+    return res.render('signup')
+  }
+  res.render('signup', { username })
+})
+
+app.get('/trackName', (req, res) => {
+  res.cookie('username', req.query.name)
+  res.redirect('/myName')
+})
+
+// Error Handling
+app.use((req, res, next) => {
+  const err = new Error("Can't find this page")
+  err.status = 404
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  res.locals.error = err
+  res.status(err.status)
+  res.render('error', { err })
+})
+
+app.listen(3000, () => {
+  console.log('The application is running on localhost:3000')
+})
